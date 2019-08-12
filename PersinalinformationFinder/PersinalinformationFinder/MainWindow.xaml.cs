20a4133a42b5b3f866472b4c62406cab6c70082c
yeojin <yeojin011028@naver.com>
2019-08-12 19:54:38 +0900

    2019-08-12 오후 수업

--- PersinalinformationFinder/PersinalinformationFinder/MainWindow.xaml.cs
+++ PersinalinformationFinder/PersinalinformationFinder/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace PersinalinformationFinder
 {
@@ -20,9 +22,38 @@ namespace PersinalinformationFinder
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string selectfolder;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public string ReadFileText(string path)
+        {
+            //파일 읽고 텍스트 추출함수
+            return File.ReadAllText(path);
+        }
+
+        private void SearchClick(object sender, RoutedEventArgs e)
+        {
+            
+            string[] fileEntries = Directory.GetFiles(selectfolder);
+
+            foreach(string fileName in fileEntries)
+            {
+
+            }
+
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            //열기 버튼 클릭 이벤트
+            FolderBrowserDialog folderopen = new FolderBrowserDialog();
+            folderopen.Description = "검색할 폴더";
+            folderopen.ShowDialog();
+
+            //선택한 폴더의 경로
+            selectfolder = folderopen.SelectedPath;
         }
     }
 }
