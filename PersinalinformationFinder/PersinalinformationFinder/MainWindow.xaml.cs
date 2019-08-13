2a141a80852e23edf0617e69f50a6bb47c816f65
yeojin011028@naver.com <yeojin011028@naver.com>
2019-08-13 19:43:53 +0900

    2019-08-13 완료

--- PersinalinformationFinder/PersinalinformationFinder/MainWindow.xaml.cs
+++ PersinalinformationFinder/PersinalinformationFinder/MainWindow.xaml.cs
@@ -40,7 +40,17 @@ namespace PersinalinformationFinder
 
             foreach(string fileName in fileEntries)
             {
+                if (System.IO.Path.GetExtension(fileName) == ".txt")
+                {
+                    String value = File.ReadAllText(fileName);
+                    string values = "";
+                    int all = DetectPhoneNumber(value, ref values);
+                    if (all > 0)
+                    {
+                        view.Items.Add(fileName);
+                    }
 
+                }
             }
 
         }
@@ -54,6 +64,51 @@ namespace PersinalinformationFinder
 
             //선택한 폴더의 경로
             selectfolder = folderopen.SelectedPath;
+
+            load.Text = selectfolder;
+        }
+        public int DetectPhoneNumber(String value, ref String values)
+        {
+            String[] patterns =
+            {
+                @"(^|\W)01[01]\d{7,8}(\W|$)",//전화번호
+                @"(^|\W)01[01]-\d{3,4}-\d{4}(\W|$)",//전화번호
+                @"[0-9]{6}-[0-9]{7}",//주민등록번호
+                @"[0-9] [0-9] [01] [0-9] [0123] [0-9]-[1234] [0-9]{6}",//주민등록번호
+                @"^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$",//이메일
+                Search.Text//검색글자
+            };
+            int count = 0;
+
+            foreach(String pattern in patterns)
+            {
+                System.Text.RegularExpressions.MatchCollection matches = System.Text.RegularExpressions.Regex.Matches(value, pattern);
+
+                foreach(System.Text.RegularExpressions.Match m in matches)
+                {
+                    if (m.Value != "")
+                    {
+                        ++count;
+                        values += m + "\n";
+                    }
+                }
+            }
+            return count;
+        }
+
+        private void DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Controls.ListView list = sender as System.Windows.Controls.ListView;
+            string va = list.SelectedItem as string;
+            String val = File.ReadAllText(va);
+            Read.Text = val;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Search.Text = "";
+            view.Items.Clear();
+            Read.Text = "";
         }
     }
 }
